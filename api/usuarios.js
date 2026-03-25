9185cd3b20968308cfe4123d133fb5c3557c6f52
Torres <ramontorresbarajas@gmail.com>
2026-03-25 00:40:08 -0600

    Fix: ahora si bien

--- api/usuarios.js
+++ api/usuarios.js
@@ -2,22 +2,19 @@ import { kv } from '@vercel/kv';
 
 export default async function handler(request, response) {
   try {
-    // Si la petición es GET (Leer usuarios)
     if (request.method === 'GET') {
       const usuarios = await kv.get('usuarios') || [];
       return response.status(200).json(usuarios);
     }
 
-    // Si la petición es POST (Guardar usuario)
     if (request.method === 'POST') {
       const nuevoUsuario = request.body;
       const usuariosActuales = await kv.get('usuarios') || [];
       usuariosActuales.push(nuevoUsuario);
       await kv.set('usuarios', usuariosActuales);
-      return response.status(200).json({ message: 'Usuario guardado' });
+      return response.status(200).json({ status: 'success' });
     }
   } catch (error) {
-    console.error(error);
-    return response.status(500).json({ error: 'Fallo la conexión con Redis', detalles: error.message });
+    return response.status(500).json({ error: error.message });
   }
 }
